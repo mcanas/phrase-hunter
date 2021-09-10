bc2802d3a2fe429f7d8a10956e86a66dc8ebea76
Miguel Canas <mcanas1981@gmail.com>
2021-09-10 10:08:14 -0400

    Phrase Hunters: adding pseudo code to game.py and phrase.py

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,1 +1,33 @@
 # Create your Game class logic in here.
+
+# Create a Game class
+
+# has an __init()__ method
+#   sets missed attribute to initial value of 0
+#   sets phrases attribute that will hold a list of Phrase instances
+#     a phrase should only contain spaces and letters
+#   sets an active_phrase attribute with initial value of None
+#   sets a guesses attribute that will hold a list of guessed letters
+
+
+# has a start() method
+#   calls welcome()
+#   starts game loop
+#     calls get_guess()
+#     adds guess to guesses list
+#     increments missed when guess is incorrect
+#     calls game_over() when missed equals 5
+
+
+# has a get_random_phrase() method
+#   returns a random Phrase from phrases list
+
+# has a welcome() method
+#   prints a welcome message
+
+# has a get_guess() method
+#   retreives and validates user input as a single letter guess
+#   store guess in guesses list
+
+# has a game_over() method
+#   prints a win or lose message
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,21 @@
 # Create your Phrase class logic here.
+
+# Create a Phrase class
+
+# has an __init__() method
+#   accepts a phrase param
+#   holds the phrase in an instance attribute
+#   phrase attribute value should be lower case
+
+
+# has a display() method
+#   prints phrase to console with guessed letters visible and unguessed letters as underscores
+#     ex: For phrase "Hello World" and guess is "o", output is: _ _ _ _ o  _ o _ _ _
+
+
+# has a check_letters() method
+#   checks to see if guessed letter is in the phrase
+
+
+# has a check_complete() method
+#   checks to see if phrase has been guessed
